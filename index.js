867ccbc6a4e16309d2d15a8a943471986e9ccc8b
bansi <bansidobariya2510@gmail.com>
2025-03-22 11:17:52 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const http = require("http");
+const fs = require("fs");
+const url = require("url");
+
+const replaceTemplate = (temp, product) => {
+  let output = temp.replace(/{%PRODUCTNAME%}/g, product.productName);
+  output = output.replace(/{%IMAGE%}/g, product.image);
+  output = output.replace(/{%PRICE%}/g, product.price);
+  output = output.replace(/{%FROM%}/g, product.from);
+  output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+  output = output.replace(/{%QUANTITY%}/g, product.quantity);
+  output = output.replace(/{%DESCRIPTION%}/g, product.description);
+  output = output.replace(/{%ID%}/g, product.id);
+
+  if (!product.organic)
+    output = output.replace(/{%NOT_ORGANIC%}/g, "not-organic");
+  return output;
+};
+
+const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
+const tempo = fs.readFileSync(`${__dirname}/templates/overview.html`, "utf-8");
+const tempc = fs.readFileSync(`${__dirname}/templates/Templet.html`, "utf-8");
+const tempp = fs.readFileSync(`${__dirname}/templates/product.html`, "utf-8");
+
+const dataObj = JSON.parse(data);
+const server = http.createServer((req, res) => {
+  console.log(req.url);
+  const pathName = req.url;
+
+  if (pathName === "/overview" || pathName === "/") {
+    res.writeHead(200, { "Content-type": "text/html" });
+    const card = dataObj.map((el) => replaceTemplate(tempc, el)).join("");
+    const output = tempo.replace(/{%PRODUCT_CARDS%}/g, card);
+    res.end(output);
+  } else if (pathName === "/api") {
+    res.writeHead(200, { "Content-type": "application/json" });
+    const productNames = dataObj.map((item) => item.productName);
+
+    res.end(JSON.stringify(productNames));
+  }
+});
+
+server.listen(3000, "127.0.0.1", () => {
+  console.log("server is running on port 3000");
+});
